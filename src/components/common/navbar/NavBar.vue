640de6119f9d8d32c60b3af27bc393d960287969
unknown <* name =*>
2021-05-12 17:30:05 +0800

    初始化项目

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style>
+.nav-bar {
+  display: flex;
+  height: 44px;
+  line-height: 44px; /*导航栏的高度一般为44，line-height可以使得文字上下居中,line-height要生效，必须要有内容*/
+  text-align: center;
+  box-shadow: 0 1px 1px rgba(100, 100, 100, 0.3);
+}
+
+.left,
+.right {
+  width: 60px;
+}
+
+.center {
+  flex: 1;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,23 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
+
 export default {
   name: "Home",
+  components: {
+    NavBar,
+  },
 };
 </script>
 
 <style>
+.home-nav {
+  background-color: var(--color-tint);
+  color: white;
+}
 </style>
