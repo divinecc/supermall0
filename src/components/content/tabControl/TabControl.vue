b236d37f20350bd086052c8f28f749fd171d22e1
unknown <* name =*>
2021-05-14 17:16:48 +0800

    初始化项目

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item, index) in titles" :key="index" class="tab-control-item">
+      <span>{{ item }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabControl",
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+
+<style>
+.tab-control {
+  display: flex;
+  text-align: center;
+  font-size: 15px;
+  height: 40px;
+  line-height: 40px;
+}
+.tab-control-item {
+  flex: 1;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,19 +2,30 @@
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+    <feature-view />
+    <tab-control :titles="['流行', '新款', '精选']" />
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+
 import { getHomeMultidata } from "network/home"; //只有default导出才能把大括号删除，但是这的这个getHomeMultidata不是default导出，所以必须加大括号
 
 export default {
   name: "Home",
   components: {
     NavBar,
+    TabControl,
     HomeSwiper,
+    RecommendView,
+    FeatureView,
   },
   data() {
     return {
@@ -34,8 +45,16 @@ export default {
 </script>
 
 <style>
+#home {
+  padding-top: 44px;
+}
 .home-nav {
   background-color: var(--color-tint);
   color: white;
+  position: fixed; /*这句话的意思是上下滑动屏幕的时候让这个导航栏不要动*/
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
 }
 </style>
